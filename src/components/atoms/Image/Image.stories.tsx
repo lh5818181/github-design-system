1464fc946cb224c47183cadb0686447f7c48b621
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-11-21 14:28:02 -0300

    feat: add Image atom

---
+++ src/components/atoms/Image/Image.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Image } from '.';
+
+const meta: Meta<typeof Image> = {
+  title: 'Atoms/Image',
+  component: Image,
+  args: {
+    src: 'https://res.cloudinary.com/dbnq26wqe/image/upload/v1700505897/others/image_j8ej2v.jpg',
+    alt: 'Avatar do usuário',
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Image>;
+
+const template: Story = {
+  render: (args) => <Image {...args} />,
+};
+
+export const Default: Story = {
+  ...template,
+};
