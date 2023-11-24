f2b9722842cebc4fc916ab4f6f4ff38d1bf40d18
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-11-24 20:02:21 -0300

    chore: move Button for atoms folder

--- src/components/atoms/Button/Button.stories.tsx
+++ src/components/atoms/Button/Button.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from '.';
 
 const meta: Meta<typeof Button> = {
-  title: 'Button',
+  title: 'Atoms/Button',
   component: Button,
   args: {
     children: 'Button',
