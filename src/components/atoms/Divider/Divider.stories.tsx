320759ab083e8d1fd624518be871b7324c9a0f3a
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-11-25 00:19:20 -0300

    feat: add Divider atom

---
+++ src/components/atoms/Divider/Divider.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Divider } from '.';
+
+const meta: Meta<typeof Divider> = {
+  title: 'Atoms/Divider',
+  component: Divider,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Divider>;
+
+const template: Story = {
+  render: () => <Divider />,
+};
+
+export const Default: Story = {
+  ...template,
+};
